f2d503c9512c9412d8fa3d1357071ba1db9a3ac6
n-andr <150958639+n-andr@users.noreply.github.com>
2025-03-02 18:50:35 +0100

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = "";
+  let addition = options.addition !== undefined ? String(options.addition) : '';
+  let separator = options.separator !== undefined ? String(options.separator) : '+';
+  let additionSeparator = options.additionSeparator !== undefined ? String(options.additionSeparator) : '|';
+  if(!options.repeatTimes) options.repeatTimes = 1;
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+  for (let i = 0; i < options.repeatTimes; i += 1){
+	result += str;
+	for (let j = 0; j < options.additionRepeatTimes; j += 1){
+		result += addition;
+		if (j < options.additionRepeatTimes - 1)
+			result += additionSeparator;
+	}
+	if (i < options.repeatTimes - 1)
+		result += separator;
+  }
+  return result;
 }
 
 module.exports = {
